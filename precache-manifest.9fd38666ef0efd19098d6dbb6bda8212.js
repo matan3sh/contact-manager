b1e6fd37633e4abc158ea06e32e2d6d66d7be189
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-06-30 14:27:57 +0300

    Deploy

---
+++ precache-manifest.9fd38666ef0efd19098d6dbb6bda8212.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e770576a8083cb8fbec6ceae88a47e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39e57d20d8d691594b70",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "83eca0f00df9f5722689",
+    "url": "/static/css/main.f03daa07.chunk.css"
+  },
+  {
+    "revision": "39e57d20d8d691594b70",
+    "url": "/static/js/2.f032ccda.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.f032ccda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83eca0f00df9f5722689",
+    "url": "/static/js/main.198beb43.chunk.js"
+  },
+  {
+    "revision": "09ef95147c6cc016fcde",
+    "url": "/static/js/runtime-main.dce2e71d.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
